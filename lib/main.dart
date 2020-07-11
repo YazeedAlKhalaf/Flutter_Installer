cf4e4af377f692c03227d7890477a3a36eadb245
Yazeed AlKhalaf <yazeedfady@gmail.com>
2020-07-11 15:33:23 +0300

    🎉 Add Window Size Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,36 @@
+import 'dart:io' show Platform;
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:flutter_installer/src/app/generated/locator/locator.dart';
 import 'package:flutter_installer/src/app/generated/router/router.gr.dart';
 import 'package:flutter_installer/src/ui/global/app_colors.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:window_size/window_size.dart' as window_size;
+import 'package:window_size/window_size.dart';
 
 main() {
   WidgetsFlutterBinding.ensureInitialized();
+
+  window_size.getWindowInfo().then((PlatformWindow window) {
+    if (window.screen != null) {
+      final screenFrame = window.screen.visibleFrame;
+      final width = math.max((screenFrame.width / 2).roundToDouble(), 800.0);
+      final height = math.max((screenFrame.height / 2).roundToDouble(), 600.0);
+      final left = ((screenFrame.width - width) / 2).roundToDouble();
+      final top = ((screenFrame.height - height) / 3).roundToDouble();
+      final frame = Rect.fromLTWH(left, top, width, height);
+      window_size.setWindowFrame(frame);
+      window_size
+          .setWindowTitle('Flutter Installer for ${Platform.operatingSystem}');
+
+      if (Platform.isMacOS) {
+        window_size.setWindowMinSize(Size(800, 600));
+        window_size.setWindowMaxSize(Size(1600, 1200));
+      }
+    }
+  });
 
   // Sets logging level
   Logger.level = Level.debug;

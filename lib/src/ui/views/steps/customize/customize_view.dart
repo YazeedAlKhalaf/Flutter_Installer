c37595c4dde61428cd323cb70229c3f4cd163ad5
Yazeed AlKhalaf <yazeedfady@gmail.com>
2020-07-12 01:38:16 +0300

    ✒ Edit Customize Step View

--- lib/src/ui/views/steps/customize/customize_view.dart
+++ lib/src/ui/views/steps/customize/customize_view.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_installer/src/ui/global/app_colors.dart';
+import 'package:flutter_installer/src/ui/global/ui_helpers.dart';
+import 'package:flutter_installer/src/ui/widgets/custom_button.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:stacked/stacked.dart';
 
 import './customize_view_model.dart';
 
 class CustomizeView extends StatelessWidget {
+  final Function() onNextPressed;
+
+  const CustomizeView({
+    @required this.onNextPressed,
+  });
+
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<CustomizeViewModel>.reactive(
@@ -14,9 +24,149 @@ class CustomizeView extends StatelessWidget {
         Widget child,
       ) {
         return Scaffold(
-          body: Center(
-            child: Text(
-              'CustomizeView',
+          body: SafeArea(
+            child: Center(
+              child: Container(
+                padding: EdgeInsets.all(blockSize(context)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Customize',
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.roboto(
+                            color: textColorBlack,
+                            fontSize: blockSize(context) * 5,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    verticalSpaceSmall(context),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Container(
+                          width: blockSize(context) * 35,
+                          child: TextField(
+                            controller: model.chooseFolderController,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(
+                                  500,
+                                ),
+                              ),
+                              hintText: 'Please Choose Installation Location',
+                              hintStyle: GoogleFonts.robotoMono(
+                                fontSize: blockSize(context) * 1.5,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            enabled: false,
+                          ),
+                        ),
+                        CustomButton(
+                          text: 'Browse',
+                          width: blockSize(context) * 15,
+                          onPressed: () {
+                            // TODO: Implement choosing directory :)
+                          },
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: blockSize(context) * 1.5),
+                    Divider(
+                      thickness: 2,
+                      color: lynchColor,
+                      indent: 30,
+                      endIndent: 30,
+                    ),
+                    SizedBox(height: blockSize(context) * 1.5),
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: blockSize(context) * 2.5,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Choose apps you need:',
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.robotoMono(
+                              fontSize: blockSize(context) * 2,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+
+                          // Choices
+                          CheckboxListTile(
+                            title: Text(
+                              'Install Visual Studio Code',
+                            ),
+                            value: model.installVisualStudioCode,
+                            onChanged: model.setInstallVisualStudioCode,
+                          ),
+                          CheckboxListTile(
+                            title: Text(
+                              'Install Android Studio',
+                            ),
+                            value: model.installAndroidStudio,
+                            onChanged: model.setInstallAndroidStudio,
+                          ),
+                          CheckboxListTile(
+                            title: Text(
+                              'Install IntelliJ IDEA',
+                            ),
+                            value: model.installIntelliJIDEA,
+                            onChanged: model.setInstallIntelliJIDEA,
+                          ),
+                          CheckboxListTile(
+                            title: Text(
+                              'Install Git',
+                            ),
+                            value: model.installGit,
+                            onChanged: model.setInstallGit,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Text(
+                          'Pro Tip: If you have any of them downloaded, uncheck it! 😎',
+                          softWrap: true,
+                          style: GoogleFonts.robotoMono(
+                            fontSize: blockSize(context) * 1.25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        CustomButton(
+                          text: 'Next',
+                          textStyle: GoogleFonts.roboto(
+                            fontSize: blockSize(context) * 2,
+                            fontWeight: FontWeight.bold,
+                            color: textColorWhite,
+                          ),
+                          buttonColor: accentColor,
+                          width: blockSize(context) * 10,
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                    Expanded(
+                      child: Container(),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
         );
--- lib/src/ui/views/steps/customize/customize_view_model.dart
+++ lib/src/ui/views/steps/customize/customize_view_model.dart
@@ -1,3 +1,34 @@
-import 'package:stacked/stacked.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_installer/src/ui/global/custom_base_view_model.dart';
 
-class CustomizeViewModel extends BaseViewModel {}
+class CustomizeViewModel extends CustomBaseViewModel {
+  final TextEditingController chooseFolderController = TextEditingController();
+
+  bool _installVisualStudioCode = false;
+  bool get installVisualStudioCode => _installVisualStudioCode;
+  void setInstallVisualStudioCode(bool newValue) {
+    _installVisualStudioCode = newValue;
+    notifyListeners();
+  }
+
+  bool _installAndroidStudio = true;
+  bool get installAndroidStudio => _installAndroidStudio;
+  void setInstallAndroidStudio(bool newValue) {
+    _installAndroidStudio = newValue;
+    notifyListeners();
+  }
+
+  bool _installIntelliJIDEA = false;
+  bool get installIntelliJIDEA => _installIntelliJIDEA;
+  void setInstallIntelliJIDEA(bool newValue) {
+    _installIntelliJIDEA = newValue;
+    notifyListeners();
+  }
+
+  bool _installGit = true;
+  bool get installGit => _installGit;
+  void setInstallGit(bool newValue) {
+    _installGit = newValue;
+    notifyListeners();
+  }
+}

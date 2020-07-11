a0b0c175c34983d65cec1c08c72a4cc25a2e82a8
Yazeed AlKhalaf <yazeedfady@gmail.com>
2020-07-11 20:41:55 +0300

    ➕ Add Step Widget

---
+++ lib/src/ui/widgets/step_widget.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_installer/src/ui/global/app_colors.dart';
+import 'package:flutter_installer/src/ui/global/ui_helpers.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+enum StepWidgetState {
+  Done,
+  Doing,
+  NotDone,
+}
+
+class StepWidget extends StatelessWidget {
+  final String stepName;
+  final StepWidgetState stepState;
+
+  const StepWidget({
+    @required this.stepName,
+    @required this.stepState,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    decideStepType() {
+      switch (stepState) {
+        case StepWidgetState.Done:
+          return Icon(
+            Icons.done,
+            size: blockSize(context) * 3,
+            color: textColorWhite,
+          );
+          break;
+        case StepWidgetState.Doing:
+          return Icon(
+            Icons.fiber_manual_record,
+            size: blockSize(context) * 3,
+            color: textColorWhite,
+          );
+          break;
+        case StepWidgetState.NotDone:
+        default:
+          return Container();
+          break;
+      }
+    }
+
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: blockSize(context) * 1.5,
+        horizontal: blockSize(context),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            width: blockSize(context) * 4,
+            height: blockSize(context) * 4,
+            child: Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                CircleAvatar(),
+                decideStepType(),
+              ],
+            ),
+          ),
+          SizedBox(width: blockSize(context) * 3),
+          Text(
+            stepName,
+            textAlign: TextAlign.start,
+            style: GoogleFonts.roboto(
+              fontSize: blockSize(context) * 2,
+              fontWeight: FontWeight.bold,
+              color: textColorWhite,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

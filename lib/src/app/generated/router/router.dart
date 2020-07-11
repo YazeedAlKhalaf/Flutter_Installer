17ab442c3d199286ad34f120ecda66c7d72ff25e
Yazeed AlKhalaf <yazeedfady@gmail.com>
2020-07-11 19:28:48 +0300

    ➕ Add Steps Base View to the Router

--- lib/src/app/generated/router/router.dart
+++ lib/src/app/generated/router/router.dart
@@ -2,12 +2,14 @@ import 'package:auto_route/auto_route_annotations.dart';
 import 'package:flutter_installer/src/ui/views/faq/faq_view.dart';
 import 'package:flutter_installer/src/ui/views/startup/startup_view.dart';
 import 'package:flutter_installer/src/ui/views/home/home_view.dart';
+import 'package:flutter_installer/src/ui/views/steps/steps_base/steps_base_view.dart';
 
 @MaterialAutoRouter(
   routes: <AutoRoute>[
     MaterialRoute(page: StartupView, initial: true),
     MaterialRoute(page: HomeView),
     MaterialRoute(page: FaqView),
+    MaterialRoute(page: StepsBaseView),
   ],
 )
 class $Router {}
--- lib/src/app/generated/router/router.gr.dart
+++ lib/src/app/generated/router/router.gr.dart
@@ -10,15 +10,18 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter_installer/src/ui/views/startup/startup_view.dart';
 import 'package:flutter_installer/src/ui/views/home/home_view.dart';
 import 'package:flutter_installer/src/ui/views/faq/faq_view.dart';
+import 'package:flutter_installer/src/ui/views/steps/steps_base/steps_base_view.dart';
 
 class Routes {
   static const String startupView = '/';
   static const String homeView = '/home-view';
   static const String faqView = '/faq-view';
+  static const String stepsBaseView = '/steps-base-view';
   static const all = <String>{
     startupView,
     homeView,
     faqView,
+    stepsBaseView,
   };
 }
 
@@ -29,6 +32,7 @@ class Router extends RouterBase {
     RouteDef(Routes.startupView, page: StartupView),
     RouteDef(Routes.homeView, page: HomeView),
     RouteDef(Routes.faqView, page: FaqView),
+    RouteDef(Routes.stepsBaseView, page: StepsBaseView),
   ];
   @override
   Map<Type, AutoRouteFactory> get pagesMap => _pagesMap;
@@ -48,6 +52,12 @@ class Router extends RouterBase {
     FaqView: (RouteData data) {
       return MaterialPageRoute<dynamic>(
         builder: (context) => FaqView(),
+        settings: data,
+      );
+    },
+    StepsBaseView: (RouteData data) {
+      return MaterialPageRoute<dynamic>(
+        builder: (context) => StepsBaseView(),
         settings: data,
       );
     },

ec52072b0730001390ab9a7e6456cb145586fa4f
Yazeed AlKhalaf <yazeedfady@gmail.com>
2020-07-11 21:45:16 +0300

    ✒ Edit Steps Base View

--- lib/src/ui/views/steps/steps_base/steps_base_view.dart
+++ lib/src/ui/views/steps/steps_base/steps_base_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_installer/src/ui/global/app_colors.dart';
+import 'package:flutter_installer/src/ui/global/ui_helpers.dart';
+import 'package:flutter_installer/src/ui/widgets/step_widget.dart';
 import 'package:stacked/stacked.dart';
 
 import './steps_base_view_model.dart';
@@ -8,15 +11,58 @@ class StepsBaseView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ViewModelBuilder<StepsBaseViewModel>.reactive(
       viewModelBuilder: () => StepsBaseViewModel(),
+      onModelReady: (StepsBaseViewModel model) => model.initialize(),
       builder: (
         BuildContext context,
         StepsBaseViewModel model,
         Widget child,
       ) {
         return Scaffold(
-          body: Center(
-            child: Text(
-              'StepsBaseView',
+          body: SafeArea(
+            child: Container(
+              child: Row(
+                children: <Widget>[
+                  // Left Side
+                  Container(
+                    width: blockSize(context) * 35,
+                    padding: EdgeInsets.all(blockSize(context)),
+                    decoration: BoxDecoration(
+                      color: primaryColor,
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        StepWidget(
+                          stepName: 'Terms of Service',
+                          stepState: model.decideStepState(0),
+                        ),
+                        StepWidget(
+                          stepName: 'Customize',
+                          stepState: model.decideStepState(1),
+                        ),
+                        StepWidget(
+                          stepName: 'Pre-installation Summary',
+                          stepState: model.decideStepState(2),
+                        ),
+                        StepWidget(
+                          stepName: 'Installing',
+                          stepState: model.decideStepState(3),
+                        ),
+                        StepWidget(
+                          stepName: 'Done!',
+                          stepState: model.decideStepState(4),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  // Right Side
+                  Expanded(
+                    child: Container(
+                      child: model.decideStepView(),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         );
--- lib/src/ui/views/steps/steps_base/steps_base_view_model.dart
+++ lib/src/ui/views/steps/steps_base/steps_base_view_model.dart
@@ -1,3 +1,64 @@
+import 'package:flutter_installer/src/ui/views/steps/customize/customize_view.dart';
+import 'package:flutter_installer/src/ui/views/steps/done/done_view.dart';
+import 'package:flutter_installer/src/ui/views/steps/installing/installing_view.dart';
+import 'package:flutter_installer/src/ui/views/steps/summary/summary_view.dart';
+import 'package:flutter_installer/src/ui/views/steps/terms_of_service/terms_of_service_view.dart';
+import 'package:flutter_installer/src/ui/widgets/step_widget.dart';
 import 'package:stacked/stacked.dart';
 
-class StepsBaseViewModel extends BaseViewModel {}
+class StepsBaseViewModel extends BaseViewModel {
+  int _currentIndex;
+  int get currentIndex => _currentIndex;
+
+  void setCurrentIndex(
+    /// `newIndex` can't be more than `4`
+    /// `4` is the maximum
+    /// `0` is the minimum
+    int newIndex,
+  ) {
+    assert(newIndex <= 4 && newIndex >= 0);
+    _currentIndex = newIndex;
+    notifyListeners();
+  }
+
+  void initialize() {
+    setCurrentIndex(0);
+  }
+
+  StepWidgetState decideStepState(int stepIndex) {
+    if (_currentIndex == stepIndex) {
+      return StepWidgetState.Doing;
+    }
+
+    if (_currentIndex > stepIndex) {
+      return StepWidgetState.Done;
+    }
+
+    return StepWidgetState.NotDone;
+  }
+
+  decideStepView() {
+    switch (_currentIndex) {
+      case 0:
+        return TermsOfServiceView(
+          onAgreePressed: () {
+            setCurrentIndex(1);
+          },
+        );
+        break;
+      case 1:
+        return CustomizeView();
+        break;
+      case 2:
+        return SummaryView();
+        break;
+      case 3:
+        return InstallingView();
+        break;
+      case 4:
+        return DoneView();
+        break;
+      default:
+    }
+  }
+}

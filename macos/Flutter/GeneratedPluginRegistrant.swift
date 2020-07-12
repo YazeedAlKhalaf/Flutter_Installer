67502a40ef31cad95133cb16f4b6406d6361b6db
Yazeed AlKhalaf <yazeedfady@gmail.com>
2020-07-12 03:05:51 +0300

    ➕ Add File Chooser Plugin

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import file_chooser
 import window_size
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileChooserPlugin.register(with: registry.registrar(forPlugin: "FileChooserPlugin"))
   WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }

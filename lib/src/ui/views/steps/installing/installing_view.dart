230ef559b306cff9efce6edbe3296434b0ef3314
Yazeed AlKhalaf <yazeedfady@gmail.com>
2020-07-12 20:55:52 +0300

    ✒ Edit Installing Step View

--- lib/src/ui/views/steps/installing/installing_view.dart
+++ lib/src/ui/views/steps/installing/installing_view.dart
@@ -1,22 +1,126 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_installer/src/ui/global/app_colors.dart';
+import 'package:flutter_installer/src/ui/global/ui_helpers.dart';
+import 'package:flutter_installer/src/ui/widgets/custom_button.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 import 'package:stacked/stacked.dart';
 
 import './installing_view_model.dart';
 
 class InstallingView extends StatelessWidget {
+  final Function() onNextPressed;
+  final Function() onCancelPressed;
+
+  const InstallingView({
+    @required this.onNextPressed,
+    @required this.onCancelPressed,
+  });
+
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<InstallingViewModel>.reactive(
       viewModelBuilder: () => InstallingViewModel(),
+      onModelReady: (InstallingViewModel model) => model.initialize(),
       builder: (
         BuildContext context,
         InstallingViewModel model,
         Widget child,
       ) {
         return Scaffold(
-          body: Center(
-            child: Text(
-              'InstallingView',
+          body: SafeArea(
+            child: Center(
+              child: Container(
+                padding: EdgeInsets.all(blockSize(context)),
+                child: ListView(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Installing',
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.roboto(
+                            color: textColorBlack,
+                            fontSize: blockSize(context) * 5,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    verticalSpaceSmall(context),
+                    Container(
+                      padding: EdgeInsets.all(blockSize(context) * 2),
+                      child: LinearPercentIndicator(
+                        lineHeight: 35,
+                        percent: model.percentage,
+                        center: Text(
+                          "${(model.percentage * 100).toInt()}%",
+                          style: GoogleFonts.roboto(
+                            fontWeight: FontWeight.bold,
+                            fontSize: blockSize(context) * 2,
+                            color: model.percentage >= 0.5
+                                ? textColorWhite
+                                : textColorBlack,
+                          ),
+                        ),
+                        progressColor: accentColor,
+                      ),
+                    ),
+                    Container(
+                      child: Text(
+                        'Downloading Flutter',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.robotoMono(
+                          fontSize: blockSize(context) * 1.5,
+                          color: lynchColor,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          CustomButton(
+                            text: 'Cancel',
+                            textStyle: GoogleFonts.roboto(
+                              fontSize: blockSize(context) * 2,
+                              color: textColorWhite,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            buttonColor: dangerColor,
+                            width: blockSize(context) * 15,
+                            onPressed: () async {
+                              bool isSure =
+                                  await model.showCancelConfirmationDialog();
+                              if (isSure) {
+                                onCancelPressed();
+                              }
+                            },
+                          ),
+                          CustomButton(
+                            text: 'Next',
+                            textStyle: GoogleFonts.roboto(
+                              fontSize: blockSize(context) * 2,
+                              color: textColorWhite,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            buttonColor: primaryColor,
+                            width: blockSize(context) * 15,
+                            onPressed: () {
+                              onNextPressed();
+                            },
+                            isButtonDisabled:
+                                model.percentage >= 1.0 ? false : true,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
         );
--- lib/src/ui/views/steps/installing/installing_view_model.dart
+++ lib/src/ui/views/steps/installing/installing_view_model.dart
@@ -1,3 +1,37 @@
-import 'package:stacked/stacked.dart';
+import 'package:flutter_installer/src/app/generated/locator/locator.dart';
+import 'package:flutter_installer/src/ui/global/custom_base_view_model.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-class InstallingViewModel extends BaseViewModel {}
+class InstallingViewModel extends CustomBaseViewModel {
+  final DialogService _dialogService = locator<DialogService>();
+
+  double _percentage;
+  double get percentage => _percentage;
+  void setPercentage(double newValue) {
+    _percentage = newValue;
+    notifyListeners();
+  }
+
+  String _currentTaskText = '';
+  String get currentTaskText => _currentTaskText;
+  void setCurrentTaskText(String newValue) {
+    _currentTaskText = newValue;
+    notifyListeners();
+  }
+
+  initialize() {
+    setPercentage(0.0);
+  }
+
+  Future<bool> showCancelConfirmationDialog() async {
+    DialogResponse dialogResponse = await _dialogService.showConfirmationDialog(
+      title: 'Are You Sure? 😢',
+      description: 'Are You Sure You Wanna Cancel This Download? 😢',
+      cancelTitle: 'No, Thanks God 🙏',
+      confirmationTitle: 'Yes, I\'m Pretty Sure 🚀',
+      dialogPlatform: DialogPlatform.Material,
+    );
+
+    return dialogResponse.confirmed;
+  }
+}

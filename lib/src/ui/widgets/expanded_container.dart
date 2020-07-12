cafb17bbb4324f77aba22a6847497c144f538795
Yazeed AlKhalaf <yazeedfady@gmail.com>
2020-07-12 23:11:58 +0300

    ➕ Add ExpandedContainer Widget

---
+++ lib/src/ui/widgets/expanded_container.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ExpandedContainer extends StatelessWidget {
+  final Widget child;
+
+  const ExpandedContainer({
+    this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        child: child ?? Container(),
+      ),
+    );
+  }
+}

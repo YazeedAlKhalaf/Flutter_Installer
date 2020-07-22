f9aba4bb55ffc9152fa0658bacbfdd5e85d06b66
Yazeed AlKhalaf <yazeedfady@gmail.com>
2020-07-22 20:56:23 +0300

    🐧 Make it work with Linux and Update Packages

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -5,7 +5,6 @@
 #include "generated_plugin_registrant.h"
 
 #include <file_chooser/file_chooser_plugin.h>
-#include <url_launcher_fde/url_launcher_plugin.h>
 #include <window_size/window_size_plugin.h>
 #include <url_launcher_linux/url_launcher_plugin.h>
 
@@ -13,9 +12,6 @@ void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) file_chooser_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "FileChooserPlugin");
   file_chooser_plugin_register_with_registrar(file_chooser_registrar);
-  g_autoptr(FlPluginRegistrar) url_launcher_fde_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "UrlLauncherPlugin");
-  url_launcher_plugin_register_with_registrar(url_launcher_fde_registrar);
   g_autoptr(FlPluginRegistrar) window_size_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "WindowSizePlugin");
   window_size_plugin_register_with_registrar(window_size_registrar);

d69d3a09946b26d7b551bf899cd0d5f260c39b55
Yazeed AlKhalaf <yazeedfady@gmail.com>
2020-07-31 00:59:37 +0300

    ➕ Add FAQ

--- lib/src/ui/views/faq/faq_view.dart
+++ lib/src/ui/views/faq/faq_view.dart
@@ -6,6 +6,7 @@ import 'package:flutter_installer/src/ui/widgets/expanded_container.dart';
 
 import 'package:stacked/stacked.dart';
 
+import '../../global/ui_helpers.dart';
 import './faq_view_model.dart';
 
 class FaqView extends StatelessWidget {
@@ -31,6 +32,43 @@ class FaqView extends StatelessWidget {
           fontWeight: FontWeight.bold,
           color: textColorWhite,
         );
+
+        _buildQuestionAndAnswer({
+          @required String question,
+          @required String answer,
+        }) {
+          return Card(
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(
+                20,
+              ),
+            ),
+            child: Container(
+              padding: EdgeInsets.all(blockSize(context) * 2),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Q: $question',
+                    style: TextStyle(
+                      fontFamily: 'RobotoMono',
+                      fontSize: blockSize(context) * 1.5,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'A: $answer',
+                    style: TextStyle(
+                      fontFamily: 'RobotoMono',
+                      fontSize: blockSize(context) * 1.5,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
 
         return Scaffold(
           body: SafeArea(
@@ -91,9 +129,28 @@ class FaqView extends StatelessWidget {
                   // Right Side
                   Expanded(
                     child: Container(
-                      child: Column(
+                      padding: EdgeInsets.all(
+                        blockSize(context) * 2,
+                      ),
+                      child: ListView(
                         children: <Widget>[
-                          // TODO(yazeed): Finish the FAQ view
+                          _buildQuestionAndAnswer(
+                            question: 'What is Flutter Installer?',
+                            answer:
+                                'Flutter Installer is a tool made by Yazeed AlKhalaf for installing Flutter and needed software to your machine or environment.',
+                          ),
+                          _buildQuestionAndAnswer(
+                            question:
+                                'Why use Flutter to build an installer for Flutter?',
+                            answer:
+                                'Flutter is great for a lot of things. This is a test to see how well it performs and is a great thing that a framework has the potential to build itself an installer :)',
+                          ),
+                          _buildQuestionAndAnswer(
+                            question:
+                                'Why does the app ask for my Sudo password? (mac only)',
+                            answer:
+                                'The app needs it to be able to add Flutter to your PATH and to copy Visual Studio Code to your applications folder.',
+                          ),
                         ],
                       ),
                     ),

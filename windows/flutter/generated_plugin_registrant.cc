2177caf711507c46e20554feb36e567653c6b8ea
yahu1031 <121003219@sastra.ac.in>
2021-03-05 13:09:26 +0530

    Linter infos and pubspec update and need help with install after choosing path🚨

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -2,17 +2,15 @@
 //  Generated file. Do not edit.
 //
 
-// clang-format off
-
 #include "generated_plugin_registrant.h"
 
-#include <file_chooser/file_chooser_plugin.h>
+#include <file_selector_windows/file_selector_plugin.h>
 #include <url_launcher_windows/url_launcher_plugin.h>
 #include <window_size/window_size_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FileChooserPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FileChooserPlugin"));
+  FileSelectorPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSelectorPlugin"));
   UrlLauncherPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherPlugin"));
   WindowSizePluginRegisterWithRegistrar(

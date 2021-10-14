9028535e5a2e04a8bfc39fe2f67e3b414582d21f
jxstxn1 <justin@phntm.xyz>
2021-10-14 16:26:55 +0200

    Add bitdojoWindow

--- lib/src/app/services/window_size_service.dart
+++ lib/src/app/services/window_size_service.dart
@@ -1,9 +1,8 @@
 import 'dart:io';
 
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/widgets.dart';
 import 'package:injectable/injectable.dart';
-import 'package:window_size/window_size.dart' as window_size;
-import 'package:window_size/window_size.dart';
 
 extension StringExtension on String {
   String capitalize() {
@@ -16,44 +15,18 @@ class WindowSizeService {
   static const double width = 1200;
   static const double height = 720;
 
-  Future<PlatformWindow> _getPlatformWindow() async {
-    return await window_size.getWindowInfo();
-  }
-
-  void _setWindowSize(PlatformWindow platformWindow) {
-    final Rect frame = Rect.fromCenter(
-      center: Offset(
-        platformWindow.frame.center.dx,
-        platformWindow.frame.center.dy,
-      ),
-      width: width,
-      height: height,
-    );
-
-    window_size.setWindowFrame(frame);
-
-    setWindowTitle(
-      'Flutter Installer for ${Platform.operatingSystem.capitalize()}',
-    );
-
-    if (Platform.isMacOS) {
-      window_size.setWindowMinSize(Size(width, height));
-      window_size.setWindowMaxSize(Size(width * 2, height * 2));
-    }
-  }
-
   Future<void> initialize() async {
-    PlatformWindow platformWindow = await _getPlatformWindow();
-
-    if (platformWindow.screen != null) {
-      if (platformWindow.screen!.visibleFrame.width != width ||
-          platformWindow.screen!.visibleFrame.height != height) {
-        _setWindowSize(platformWindow);
+    doWhenWindowReady(() {
+      const initialSize = Size(height, width);
+      if (Platform.isMacOS) {
+        appWindow.minSize = Size(width, height);
+        appWindow.maxSize = Size(width * 2, height * 2);
+      } else {
+        appWindow.minSize = initialSize;
+        appWindow.size = initialSize;
       }
-    }
-  }
-
-  void setWindowTitle(String title) {
-    window_size.setWindowTitle(title);
+      appWindow.title = 'Flutter Installer for ${Platform.operatingSystem.capitalize()}';
+      appWindow.show();
+    });
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,16 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import bitsdojo_window_macos
 import file_selector_macos
 import path_provider_macos
 import shared_preferences_macos
 import url_launcher_macos
-import window_size
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  BitsdojoWindowPlugin.register(with: registry.registrar(forPlugin: "BitsdojoWindowPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
 }

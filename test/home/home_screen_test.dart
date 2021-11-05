5658ac0d7cbae10799e84d994a17ff73425d95e1
Yazeed AlKhalaf <yazeedfady@gmail.com>
2021-11-06 00:08:17 +0300

    ❌ Remove unused import

--- test/home/home_screen_test.dart
+++ test/home/home_screen_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_installer/faq/faq_screen.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 

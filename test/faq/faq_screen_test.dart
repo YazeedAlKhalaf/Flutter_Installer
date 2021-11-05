c2c91e0df77a04a38bf60fbf4e51a9e398e84207
Yazeed AlKhalaf <yazeedfady@gmail.com>
2021-11-06 00:07:36 +0300

    🧪 Widget test faq screen

---
+++ test/faq/faq_screen_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_installer/faq/faq_screen.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  Widget buildFaqScreen() {
+    return const MaterialApp(
+      home: FaqScreen(),
+    );
+  }
+
+  testWidgets(
+    "should render correctly",
+    (WidgetTester tester) async {
+      await tester.pumpWidget(buildFaqScreen());
+      await tester.pumpAndSettle();
+
+      expect(find.byType(FaqScreen), findsOneWidget);
+    },
+  );
+}

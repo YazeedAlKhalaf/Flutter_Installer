8a10b09995c4d7b3539c3bdf057e539cee48405d
Yazeed AlKhalaf <yazeedfady@gmail.com>
2021-11-06 00:01:45 +0300

    🧪 Widget test question answer tile

---
+++ test/faq/widgets/question_answer_tile_test.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_installer/faq/widgets/question_answer_tile.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  Widget buildQuestionAnswerTile() {
+    return const MaterialApp(
+      home: Scaffold(
+        body: QuestionAnswerTile(
+          question: "question",
+          answer: "answer",
+        ),
+      ),
+    );
+  }
+
+  testWidgets(
+    "should render correctly.",
+    (WidgetTester tester) async {
+      await tester.pumpWidget(buildQuestionAnswerTile());
+
+      expect(find.byType(QuestionAnswerTile), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    "should find question text.",
+    (WidgetTester tester) async {
+      await tester.pumpWidget(buildQuestionAnswerTile());
+
+      final Finder questionTextFinder = find.text(
+        "Q: question",
+      );
+      expect(questionTextFinder, findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    "should find answer text.",
+    (WidgetTester tester) async {
+      await tester.pumpWidget(buildQuestionAnswerTile());
+
+      final Finder answerTextFinder = find.text(
+        "A: answer",
+      );
+      expect(answerTextFinder, findsOneWidget);
+    },
+  );
+}

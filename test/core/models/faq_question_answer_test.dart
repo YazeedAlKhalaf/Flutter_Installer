f7691487087a3c51e04e136109ed7e59292173fc
Yazeed AlKhalaf <yazeedfady@gmail.com>
2021-11-05 23:53:39 +0300

    🧪 Test faq question answer

---
+++ test/core/models/faq_question_answer_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_installer/core/models/faq_question_answer.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test(
+    "should create a FaqQuestionAnswer model "
+    "and check the values are right.",
+    () {
+      const FaqQuestionAnswer faqQuestionAnswer = FaqQuestionAnswer(
+        question: "question",
+        answer: "answer",
+      );
+
+      expect(faqQuestionAnswer, isA<FaqQuestionAnswer>());
+
+      expect(faqQuestionAnswer.question, equals("question"));
+      expect(faqQuestionAnswer.answer, equals("answer"));
+    },
+  );
+}

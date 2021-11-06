411d4e380f32f98b2ef8b5e8bc50298832879668
Yazeed AlKhalaf <yazeedfady@gmail.com>
2021-11-06 14:27:24 +0300

    🧪 Widget test app checkbox tile

---
+++ test/customize/widgets/app_checkbox_tile_test.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_installer/customize/widgets/app_checkbox_tile.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  Widget buildAppCheckboxTile({
+    bool value = false,
+  }) {
+    return MaterialApp(
+      home: Scaffold(
+        body: AppCheckboxTile(
+          title: "title",
+          value: value,
+          onChanged: (_) {},
+        ),
+      ),
+    );
+  }
+
+  testWidgets(
+    "should render correctly.",
+    (WidgetTester tester) async {
+      await tester.pumpWidget(buildAppCheckboxTile());
+
+      expect(find.byType(AppCheckboxTile), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    "should find one app check box tile and press it.",
+    (WidgetTester tester) async {
+      await tester.pumpWidget(buildAppCheckboxTile());
+
+      expect(find.byType(AppCheckboxTile), findsOneWidget);
+
+      await tester.tap(find.byType(AppCheckboxTile));
+    },
+  );
+}
